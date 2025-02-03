11a894f47285c42cbf1571d94127c67b8211b8a1
PatoProgramador <skpates18@gmail.com>
2025-02-02 19:44:41 -0500

    First commit

---
+++ src/main/java/com/palfilo/demo/FirstVersionApplication.java
@@ -0,0 +1,13 @@
+package com.palfilo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstVersionApplication.class, args);
+	}
+
+}
