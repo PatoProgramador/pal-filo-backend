de32f2941e238f67793a394aa424a01f967bf7f2
PatoProgramador <skpates18@gmail.com>
2025-03-30 11:41:29 -0500

    Enable cors in bean

--- src/main/java/com/palfilo/demo/infra/security/SecurityConfigurations.java
+++ src/main/java/com/palfilo/demo/infra/security/SecurityConfigurations.java
@@ -46,6 +46,18 @@ public class SecurityConfigurations {
     }
 
     @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList(corsAllowedOrigins));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
