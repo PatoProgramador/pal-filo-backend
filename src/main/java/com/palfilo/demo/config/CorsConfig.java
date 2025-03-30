b833f5c2b440931fbf0bc4eb7d596a99265f59fc
PatoProgramador <skpates18@gmail.com>
2025-03-30 11:26:59 -0500

    Fix cors to all urls

--- src/main/java/com/palfilo/demo/config/CorsConfig.java
+++ src/main/java/com/palfilo/demo/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.palfilo.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
