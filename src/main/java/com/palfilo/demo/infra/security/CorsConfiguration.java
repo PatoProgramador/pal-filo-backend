37c02f18cb76c8e54a80a0a03a4542c7be5e3883
PatoProgramador <skpates18@gmail.com>
2025-03-30 19:37:11 -0500

    Add cors config

---
+++ src/main/java/com/palfilo/demo/infra/security/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.palfilo.demo.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
